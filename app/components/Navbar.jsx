0fdffc4ad6263c8f7f22f57b69a379a1ede3655a
ahmedaryf <ahmed.areef@hotmail.com>
2023-08-02 18:03:59 +0500

    transparent navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,7 +7,7 @@ import ThemeToggle from './ThemeToggle'
 
 function Navbar() {
   return (
-    <div className='bg-red-50 dark:bg-slate-400 py-4 w-screen flex justify-between items-center fixed'>
+    <div className='bg-red-50/50 dark:bg-slate-400/70 py-4 w-screen flex justify-between items-center fixed'>
         <Link href={'/'}><Image src={logo} alt='Logo' className=' w-10 md:w-12 ms-6 ' /></Link> 
 
         <div className='flex items-center'>
